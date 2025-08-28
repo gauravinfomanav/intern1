eb2c66d7e722dd53edd17e230111c98e774e72ac
Gaurav_musaffa <gaurav.malode@musaffa.com>
2025-08-28 13:02:56 +0530

    chore: regenerate platforms (modern embedding) and enable desktop

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/gaurav/Downloads/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/gaurav/Documents/Test_project/Screen1"
+export "FLUTTER_APPLICATION_PATH=/Users/gaurav/Documents/Test_project/intern1"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
